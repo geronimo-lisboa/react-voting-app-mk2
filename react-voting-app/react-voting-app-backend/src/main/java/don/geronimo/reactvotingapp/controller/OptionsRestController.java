de7ffbd61da003f6cc273962327cebd4d624676c
geronimo-lisboa <erikaeluciano2016@gmail.com>
2018-07-16 20:46:24 -0300

    o projeto do spring deve ser aberto como um projeto separado e não como um modulo do projeto do react^

--- react-voting-app/react-voting-app-backend/src/main/java/don/geronimo/reactvotingapp/controller/OptionsRestController.java
+++ react-voting-app/react-voting-app-backend/src/main/java/don/geronimo/reactvotingapp/controller/OptionsRestController.java
@@ -1,9 +1,15 @@
 package don.geronimo.reactvotingapp.controller;
 
+import don.geronimo.reactvotingapp.model.Option;
 import don.geronimo.reactvotingapp.services.OptionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("/votingoptions")
@@ -12,5 +18,44 @@ public class OptionsRestController {
     @Autowired
     public OptionsRestController(OptionService optionService){
         this.optionService = optionService;
+    }
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<Option> getAllOptions(){
+        return optionService.getAll();
+    }
+
+    @PostMapping
+    public ResponseEntity newOption(@RequestBody Option newOp){
+        Option createdOption = optionService.saveOption(newOp);
+        if(createdOption==null){
+            return ResponseEntity.noContent().build();
+        }
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(createdOption.getId())
+                .toUri();
+        return ResponseEntity.created(location).build();
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Option updateOption(@PathVariable String id, @RequestBody Option updatedOption){
+        Option option = optionService.findById(id);
+        if(option == null){
+            //TODO: eu sei que não pode simplesmente estourar exceção aqui. Como retornar esse erro direito?
+        }
+        updatedOption.setId(option.getId());
+        return optionService.updateOption(updatedOption);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteOption(@PathVariable String id){
+        Option op = optionService.findById(id);
+        if(op == null){
+            //TODO: eu sei que não pode simplesmente estourar exceção aqui. Como retornar esse erro direito?
+            throw new RuntimeException("temp");
+        }else{
+            optionService.deleteOptionById(id);
+            return new ResponseEntity("Opção excluída", HttpStatus.OK);
+        }
     }
 }
--- react-voting-app/react-voting-app-backend/src/main/java/don/geronimo/reactvotingapp/services/OptionService.java
+++ react-voting-app/react-voting-app-backend/src/main/java/don/geronimo/reactvotingapp/services/OptionService.java
@@ -1,7 +1,47 @@
 package don.geronimo.reactvotingapp.services;
 
+import don.geronimo.reactvotingapp.model.Option;
+import don.geronimo.reactvotingapp.persistance.OptionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class OptionService {
+    private OptionRepository optionRepository;
+    @Autowired
+    public OptionService(OptionRepository optionRepository){
+        this.optionRepository = optionRepository;
+    }
+
+    public List<Option> getAll(){
+        return optionRepository.findAll();
+    }
+
+
+    public Option saveOption(Option newOp) {
+        return optionRepository.save(newOp);
+    }
+
+    public Option findById(String id) {
+        Optional<Option> result = optionRepository.findById(id);
+        if(result.isPresent())
+            return result.get();
+        else
+            return null;
+    }
+
+    public Option updateOption(Option updatedOption) {
+        return optionRepository.save(updatedOption);
+    }
+
+    public void deleteOptionById(String id) {
+        optionRepository.deleteById(id);
+    }
+
+    public Long getCount(){
+        return optionRepository.count();
+    }
 }
