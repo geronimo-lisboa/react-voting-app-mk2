b75ac820613c1a1cbf0ea761699f42089509298f
geronimo-lisboa <erikaeluciano2016@gmail.com>
2018-07-16 10:34:04 -0300

    tentando por o bootstrap-react

--- react-voting-app/src/App.js
+++ react-voting-app/src/App.js
@@ -5,26 +5,60 @@ import './App.css';
 class AppTitle extends Component{
     render(){
         return(
-          <div>
+          <div >
               Voting App - Teste de app react
           </div>
         );
     }
 }
 
+class AOption extends Component {
+    render(){
+        console.log(this.props);
+        return(
+            <div>
+                <span>{this.props.name}</span>
+                <span>{this.props.votes}</span>
+            </div>
+        );
+    }
+}
+
 class App extends Component {
+    ///Cria o componente principal da app com zero opções
+    ///TODO:Puxar as opções do serviço rest a ser criado
     constructor(props){
         super(props);
-        //Seta o estado inicial, com zero opções
         this.state = {
             options : []
         }
     }
+
+    ///TODO:Puxar as opções do serviço rest a ser criado
+    componentDidMount(){
+        this.setState({options:[
+                {
+                    name:"Able",
+                    votes:10
+                },
+                {
+                    name:"Baker",
+                    votes:5
+                }
+            ]});
+    }
+    ///Renderiza o componente principal da aplicação
     render() {
-        console.log(this.state);
+        //Ordena a lista por opções
+        const sortedOptions = this.state.options.sort( (a,b)=>(b.votes - a.votes) );
+        //Mapeia os dados para componentes de tela
+        const options = sortedOptions.map( (option)=>{
+            return <AOption name={option.name} votes={option.votes}/>
+        });
         return (
         <div>
             <AppTitle/>
+            {options}
         </div>
     );
   }
